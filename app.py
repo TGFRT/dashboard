c439efc61181f6c840dbfdfa9abef090282a8ee0
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-10-01 21:25:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if st.button("Iniciar Sesión"):
     if celular_input in dfUsuarios['celular'].values:
         fila = dfUsuarios[dfUsuarios['celular'] == celular_input]
         if fila['contrasena'].values[0] == contrasena:
-            nombre_usuario = fila['Nombre'].values[0]  # Ajusta 'Nombre' según el nombre de tu columna
+            nombre_usuario = fila['nombre'].values[0]  # Ajusta 'Nombre' según el nombre de tu columna
             st.success(f"Inicio de sesión exitoso. Bienvenido, {nombre_usuario}!")
         else:
             st.error("Contraseña incorrecta.")
