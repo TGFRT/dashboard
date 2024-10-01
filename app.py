813b65ca34b8bbd3172a81127fc7bf5706a015d6
TGFRT <68918738+TGFRT@users.noreply.github.com>
2024-10-01 22:28:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 
@@ -76,7 +75,7 @@ st.markdown("""
 
 # Verificar si el usuario ya ha iniciado sesión
 if 'nombre_usuario' in st.session_state:
-    st.success(f"Hola {st.session_state['nombre_usuario']}... tus datos fueron cargados correctamente.", icon="✅")
+    st.success(f"Hola {st.session_state['nombre_usuario']}... tus datos fueron cargados correctamente. Vuelve a presionar iniciar sesión.", icon="✅")
 else:
     # Sección de inicio de sesión
     st.markdown("<h2 style='text-align: left;'>Iniciar Sesión ⭐</h2>", unsafe_allow_html=True)
@@ -93,7 +92,7 @@ else:
                 # Guardar datos en la sesión
                 st.session_state['nombre_usuario'] = fila['nombre'].values[0]
                 # Mostrar el saludo
-                st.success(f"Hola {st.session_state['nombre_usuario']}... tus datos fueron cargados correctamente.", icon="✅")
+                st.success(f"Hola {st.session_state['nombre_usuario']}... tus datos fueron cargados correctamente. Vuelve a presionar iniciar sesión.", icon="✅")
             else:
                 st.error("Contraseña incorrecta.", icon="❌")
         else:
